45f7c76bed23731a9e32839f4048c52163cdcd40
aisense <aiautou@gmail.com>
2026-02-19 18:16:35 +0900

    chore: Clean up broken paths and fix eslint-config-next version

--- src/app/demo/phase-2/hero-section/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroSection } from '@/components/sections/HeroSection'
-
-export default function HeroSectionDemoPage() {
-    return (
-        <div className="min-h-screen bg-white">
-            <HeroSection />
-        </div>
-    )
-}
